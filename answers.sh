e4d14c70a72f02db47196b98f434d971a00fa436
Abdullatif-Fk <abul647.fk@gmail.com>
2020-10-16 14:11:11 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -1,4 +1,4 @@
-select * from students;
+SELECT * from students;
 
 SELECT name from students;
 
@@ -19,5 +19,30 @@ WHERE name="Basma";
 UPDATE students 
 SET Points=190
 WHERE name="Alex";
+
+
+CREATE TABLE graduates (
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name varchar(255),
+    Age INT,
+    Gender varchar(255),
+    Points INT,
+	Graduation varchar(255)
+	
+);
+
+
+INSERT INTO graduates  ( Name, Age, Gender,Points)
+SELECT  Name, Age, Gender,Points
+FROM students 
+WHERE Name="Layal";
+
+UPDATE graduates
+SET Graduation = "08/09/2018"
+WHERE Name="Layal";
+
+DELETE FROM students WHERE Name="Layal";
+
+
 
 
